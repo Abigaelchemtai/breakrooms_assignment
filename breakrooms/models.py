941407461425aa81a0133b0ae91b3e03bba4c22e
Abigael chemtai <abigaelchemtai8@gmail.com>
2023-10-31 16:51:42 +0300

    break3

--- breakrooms/models.py
+++ breakrooms/models.py
@@ -1,3 +1,7 @@
+
 from django.db import models
 
 # Create your models here.
+
+class Instagram(models.Models):
+    pass
--- breakrooms/views.py
+++ breakrooms/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Instagram
 # Create your views here.
+
+def insta(request):
+    pass
